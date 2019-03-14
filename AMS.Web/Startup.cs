68b809ff22d85fedde6b8b3fd0468ff0e62fd48a
Nazar Homeniuk <Nazar_Homeniuk@epam.com>
2019-03-14 15:16:19 +0200

    Added external facebook login.

--- AMS.Web/Startup.cs
+++ AMS.Web/Startup.cs
@@ -42,6 +42,12 @@ namespace AMS.Web
                 .AddDefaultUI(UIFramework.Bootstrap4)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
 
+            services.AddAuthentication().AddFacebook(facebookOptions =>
+            {
+                facebookOptions.AppId = Configuration["Authentication:Facebook:AppId"];
+                facebookOptions.AppSecret = Configuration["Authentication:Facebook:AppSecret"];
+            });
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
