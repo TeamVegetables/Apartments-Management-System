9916881fe7322f2ae0767c6333df7de1369f0e7f
IvanPilyak <pilyak.vanya@gmail.com>
2019-05-09 13:30:10 +0300

    Added design to createPaymentMethod in PaymentController.

--- AMS.Web/ViewModels/Payments/CreatePaymentViewModel.cs
+++ AMS.Web/ViewModels/Payments/CreatePaymentViewModel.cs
@@ -5,5 +5,7 @@ namespace AMS.Web.ViewModels.Payments
     public class CreatePaymentViewModel
     {
         public decimal Sum { get; set; }
+
+        public DateTime DeadLine { get; set; }
     }
 }
