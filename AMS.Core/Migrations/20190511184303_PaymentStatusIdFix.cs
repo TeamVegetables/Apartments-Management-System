6888b7d7e564ebc81e0b3f1cd7a19b8d582b9634
Ivan Freiuk <Ivan_Freiuk@epam.com>
2019-05-11 22:29:08 +0300

    Fixed ui part.

---
+++ AMS.Core/Migrations/20190511184303_PaymentStatusIdFix.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AMS.Core.Migrations
+{
+    public partial class PaymentStatusIdFix : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
