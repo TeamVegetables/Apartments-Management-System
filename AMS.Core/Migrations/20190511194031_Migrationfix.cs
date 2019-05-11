576698c87acad8a8a94c523fbfa5450317182dde
Ivan Freiuk <Ivan_Freiuk@epam.com>
2019-05-11 22:54:54 +0300

    Added migration.

---
+++ AMS.Core/Migrations/20190511194031_Migrationfix.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AMS.Core.Migrations
+{
+    public partial class Migrationfix : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
