7cdf365902a0458d6c53667118e9f8bd178bbc57
Nazar Homeniuk <gomenyuknazar@gmail.com>
2019-05-15 00:42:56 +0300

    Added service tests.

---
+++ AMS.Tests/Services/ApartmentServiceTests.cs
@@ -0,0 +1,104 @@
+﻿using System.Threading.Tasks;
+using AMS.Core.Interfaces;
+using AMS.Core.Models;
+using AMS.Services.Services;
+using Moq;
+using NUnit.Framework;
+
+namespace AMS.Tests.Services
+{
+    [TestFixture]
+    public class ApartmentServiceTests
+    {
+        private Mock<IUnitOfWork> uowMock;
+        private Mock<IApartmentRepository> repositoryMock;
+
+        [SetUp]
+        public void SetUp()
+        {
+            uowMock = new Mock<IUnitOfWork>();
+            repositoryMock = new Mock<IApartmentRepository>();
+            uowMock.Setup(u => u.Apartments).Returns(repositoryMock.Object);
+        }
+
+        [Test]
+        public async Task GetApartmentAsync()
+        {
+            //Arrange
+            var service = new ApartmentService(uowMock.Object);
+            //Act
+            await service.GetApartmentAsync(0);
+            //Assert
+            repositoryMock.Verify(r => r.GetAsync(It.Is<int>(v => v == 0)), Times.Once);
+        }
+
+        [Test]
+        public void GetApartmentWithUsers()
+        {
+            //Arrange
+            var service = new ApartmentService(uowMock.Object);
+            //Act
+            service.GetApartmentWithUsers(2);
+            //Assert
+            repositoryMock.Verify(r => r.GetWithUsers(It.Is<int>(v => v == 2)), Times.Once);
+        }
+
+        [Test]
+        public void GetAllWithUsers()
+        {
+            //Arrange
+            var service = new ApartmentService(uowMock.Object);
+            //Act
+            service.GetAllWithUsers();
+            //Assert
+            repositoryMock.Verify(r => r.GetAllWithUsers(), Times.Once);
+        }
+
+        [Test]
+        public async Task GetAllApartmentsAsync()
+        {
+            //Arrange
+            var service = new ApartmentService(uowMock.Object);
+            //Act
+            await service.GetAllApartmentsAsync();
+            //Assert
+            repositoryMock.Verify(r => r.GetAllAsync(), Times.Once);
+        }
+
+        [Test]
+        public async Task AddApartmentAsync()
+        {
+            //Arrange
+            var service = new ApartmentService(uowMock.Object);
+            //Act
+            await service.AddApartmentAsync(new Apartment());
+            //Assert
+            repositoryMock.Verify(r => r.AddAsync(It.IsAny<Apartment>()), Times.Once);
+            uowMock.Verify(u => u.SaveAsync(), Times.Once);
+        }
+
+        [Test]
+        public async Task UpdateApartmentAsync()
+        {
+            //Arrange
+            var service = new ApartmentService(uowMock.Object);
+            //Act
+            await service.UpdateApartmentAsync(new Apartment());
+            //Assert
+            repositoryMock.Verify(r => r.UpdateAsync(It.IsAny<Apartment>()), Times.Once);
+            uowMock.Verify(u => u.Save(), Times.Once);
+        }
+
+        [Test]
+        public async Task RemoveApartmentAsync()
+        {
+            //Arrange
+            var service = new ApartmentService(uowMock.Object);
+            //Act
+            await service.RemoveApartmentAsync(new Apartment());
+            //Assert
+            repositoryMock.Verify(r => r.RemoveAsync(It.IsAny<Apartment>()), Times.Once);
+            uowMock.Verify(u => u.SaveAsync(), Times.Once);
+        }
+    }
+}
---
+++ AMS.Tests/Services/RequestServiceTests.cs
@@ -0,0 +1,104 @@
+﻿using System.Threading.Tasks;
+using NUnit.Framework;
+using AMS.Core.Interfaces;
+using AMS.Core.Models;
+using AMS.Services.Services;
+using Moq;
+
+namespace AMS.Tests.Services
+{
+    [TestFixture]
+    public class RequestServiceTests
+    {
+        private Mock<IUnitOfWork> uowMock;
+        private Mock<IRequestRepository> repositoryMock;
+
+        [SetUp]
+        public void SetUp()
+        {
+            uowMock = new Mock<IUnitOfWork>();
+            repositoryMock = new Mock<IRequestRepository>();
+            uowMock.Setup(u => u.Requests).Returns(repositoryMock.Object);
+        }
+
+        [Test]
+        public async Task GetRequestAsync()
+        {
+            //Arrange
+            var service = new RequestService(uowMock.Object);
+            //Act
+            await service.GetRequestAsync(0);
+            //Assert
+            repositoryMock.Verify(r => r.GetAsync(It.Is<int>(v => v == 0)), Times.Once);
+        }
+
+        [Test]
+        public async Task GetAllRequestsAsync()
+        {
+            //Arrange
+            var service = new RequestService(uowMock.Object);
+            //Act
+            await service.GetAllRequestsAsync();
+            //Assert
+            repositoryMock.Verify(r => r.GetAllAsync(), Times.Once);
+        }
+
+        [Test]
+        public async Task GetRequestsByInhabitantId()
+        {
+            //Arrange
+            var service = new RequestService(uowMock.Object);
+            //Act
+            await service.GetRequestsByInhabitantId("test");
+            //Assert
+            repositoryMock.Verify(r => r.GetRequestsByInhabitantId(It.Is<string>(v => v == "test")), Times.Once);
+        }
+
+        [Test]
+        public async Task GetRequestsByResolverId()
+        {
+            //Arrange
+            var service = new RequestService(uowMock.Object);
+            //Act
+            await service.GetRequestsByResolverId("test");
+            //Assert
+            repositoryMock.Verify(r => r.GetRequestsByResolverId(It.Is<string>(v => v == "test")), Times.Once);
+        }
+
+        [Test]
+        public async Task AddRequestAsync()
+        {
+            //Arrange
+            var service = new RequestService(uowMock.Object);
+            //Act
+            await service.AddRequestAsync(new Request());
+            //Assert
+            repositoryMock.Verify(r => r.AddAsync(It.IsAny<Request>()), Times.Once);
+            uowMock.Verify(u => u.SaveAsync());
+        }
+
+        [Test]
+        public async Task UpdateRequestAsync()
+        {
+            //Arrange
+            var service = new RequestService(uowMock.Object);
+            //Act
+            await service.UpdateRequestAsync(new Request());
+            //Assert
+            repositoryMock.Verify(r => r.UpdateAsync(It.IsAny<Request>()), Times.Once);
+            uowMock.Verify(u => u.SaveAsync());
+        }
+
+        [Test]
+        public async Task RemoveRequestAsync()
+        {
+            //Arrange
+            var service = new RequestService(uowMock.Object);
+            //Act
+            await service.RemoveRequestAsync(new Request());
+            //Assert
+            repositoryMock.Verify(r => r.RemoveAsync(It.IsAny<Request>()), Times.Once);
+            uowMock.Verify(u => u.SaveAsync());
+        }
+    }
+}
